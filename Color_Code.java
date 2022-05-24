d085938f6f79048b35f929a9cab4c67c4ac53504
Rajesh-14s <96577950+Rajesh-14s@users.noreply.github.com>
2022-05-24 19:53:52 +0530

    import java.util.Scanner; class c_c {     public static void main(String ram[])     {         char ch;         Scanner sc=new Scanner(System.in);         ch=sc.next().charAt(0);         switch(ch)         {             case'v':             case'V':                 System.out.print("Violet");                 break;             case'i':             case'I':                 System.out.print("Indigo");                 break;             case'B':             case'b':                 System.out.print("Blue");                 break;             case'G':             case'g':                 System.out.print("Green");                 break;             case'Y':             case'y':                 System.out.print("Yellow");                 break;             case'O':             case'o':                 System.out.print("Orange");                 break;             case'R':             case'r':                 System.out.print("Red");                 break;             default:                 System.out.print("-1");         }     } }

--- Color_Code.java
+++ Color_Code.java
@@ -1,44 +1,43 @@
 import java.util.Scanner;
-class color_code
+class c_c
 {
-    public static void main(String args[])
+    public static void main(String ram[])
     {
         char ch;
         Scanner sc=new Scanner(System.in);
         ch=sc.next().charAt(0);
         switch(ch)
         {
-            case'V':
             case'v':
+            case'V':
                 System.out.print("Violet");
                 break;
-            case'I':
             case'i':
+            case'I':
                 System.out.print("Indigo");
                 break;
-            case'b':
             case'B':
+            case'b':
                 System.out.print("Blue");
                 break;
-            case'g':
             case'G':
+            case'g':
                 System.out.print("Green");
                 break;
-            case'y':
             case'Y':
-                System.out.print("Yello");
+            case'y':
+                System.out.print("Yellow");
                 break;
-            case'o':
             case'O':
+            case'o':
                 System.out.print("Orange");
                 break;
-            case'r':
             case'R':
+            case'r':
                 System.out.print("Red");
                 break;
             default:
-            System.out.print("-1");
-                
+                System.out.print("-1");
         }
     }
 }
